8f26ea007091f584b6c5104acb21fdd1035030ff
ayunas <ayunas@gmail.com>
2019-03-21 11:29:01 -0500

    imported the green socks TweenMax CDN and applied animations to the selfies.  in tinkering stages. pseduocoded the logic for the gsap animations

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const form = document.querySelector('form');
 const buttons = document.getElementsByClassName('button');
 const formBtn = buttons[3];
 
-console.log(formBtn);
+
 
 connect.children[1].addEventListener('click', () => {
      connect.children[0].style.display = 'none';
@@ -16,6 +16,27 @@ formBtn.addEventListener('click', () => {
     connect.children[1].style.display = 'block';
     form.style.display = 'none';
 })
+
+
+//click on a selfie.  
+//expand the selfie
+//opacity to the remaining selfies
+//fade in a bio for that member
+
+//click on the selfie again
+//shrink it.
+//remove opacity from the remaining elements
+
+const members = document.querySelectorAll('.member');
+
+members.forEach(member => TweenMax.to('.member', 5, {opacity : '.2'}));
+
+
+//
+//members.forEach( member => 
+//TweenMax.to(member, 2, {width:'500px'}));
+
+
 
 
 
