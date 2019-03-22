088e1bd6eb95349eee662b9658367ca032e3db61
ayunas <ayunas@gmail.com>
2019-03-21 23:13:48 -0500

    styled layout for the selfie animations using click handlers.

--- app.js
+++ app.js
@@ -12,38 +12,10 @@ talkBtn.addEventListener('click', () => {
     talkBtn.style.display = 'none';
 });
 
-
-//const connect = document.querySelector('address');
-//const form = document.querySelector('form');
-//const buttons = document.getElementsByClassName('button');
-//const formBtn = buttons[3];
-//console.log(connect);
-//console.log(form);
-//console.log(buttons);
-//console.log(formBtn);
-//
-//connect.children[1].addEventListener('click', () => {
-//     connect.children[0].style.display = 'none';
-//     connect.children[1].style.display = 'none';
-//     form.style.display = 'block';
-//});
-//
-//formBtn.addEventListener('click', () => {
-//    connect.children[0].style.display = 'block';
-//    connect.children[1].style.display = 'block';
-//    form.style.display = 'none';
-//})
-
-
 //click on a selfie.  
 //scale the selfie
 //opacity to the remaining selfies
 //fade in a bio for that member
-
-
-
-
-
 
 const members = document.querySelectorAll('.member');
 const articles = document.querySelectorAll('.bio');
@@ -54,10 +26,7 @@ members.forEach( member => {
         let clicked = member;
         
         let bioID = member.dataset.bio;
-        console.log(bioID);
-        
         let bioArticle = document.querySelector(`article[data-bio='${bioID}']`);
-        console.log(bioArticle);
         
         articles.forEach( article => {
             if (article === bioArticle) {
@@ -66,62 +35,53 @@ members.forEach( member => {
                 article.classList.remove('clicked');
             }
         } );
-//        
-//        bioArticle.style.display = 'inline-block';
-//        
-//        let animation = new TimelineLite();
-//     
-//        animation .to(member, 1, {scale: 1.3})
-//            .from(bio, 1, {opacity: 0});
+            
             TweenMax.to(member, 1, {scale: 1.3});
-            TweenMax.from(bioArticle, 1, {opacity: 0, delay:1});
+            TweenMax.from(bioArticle, 2, {opacity: 0, delay:1});
+        
+            if (bioID === 'amir')  {
+                TweenMax.to(bioArticle, 1, {y: -400});
+            }
+        
+            if (bioID === 'cynthia') {
+                TweenMax.to(bioArticle, 1, {y: -400});
+            }
+        
+            if (bioID === 'will') {
+                TweenMax.to(bioArticle, 1, {y: -400, x: 300});
+            }
+        
+            if (bioID === 'hargo') {
+                TweenMax.to(bioArticle, 1, {y: -400, x: -300});
+            }          
         
         members.forEach( member => {
             if (member !== clicked) {
                 TweenMax.to(member, 1, {opacity: .2, scale: .5});
-             member.classList.remove('clicked');
-//             bioArticle.classList.remove('clicked');
+                member.classList.remove('clicked');
                 
             } else {
                 TweenMax.to(member, 1, {opacity: 1});
                 member.classList.add('clicked');
-//                bioArticle.style.display = 'inline-block';
-//                bioArticle.classList.add('clicked');
             }
         });
+        
+        console.log(clicked);
+        clicked.addEventListener('click',() => {
+            clicked.classList.remove('clicked');
+            bioArticle.classList.remove('clicked');
+            TweenMax.to(clicked, 1, {scale:1});
+        });
+        
     });
 });
 
 
-//click on a selfie
-//fade in the article box
-
-                            
-//                            
-//    let clicked = member;
-//    
-//    members.forEach( member => {
-//        if (member !== clicked) {
-//            TweenMax.to(member, 1, {opacity: .2});
-//        }
-//    }));
-
-
-
-
-//
-//members.forEach(member => TweenMax.to('.member', 3, {opacity : '.2', x:-300, rotation: 180, scaleY: 1}));
-
-//
-//members.forEach( member => 
-//TweenMax.to(member, 2, {width:'500px'}));
 
 
 
 
 
-                         
-                        
 
 
 
